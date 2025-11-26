4c530167b59a0aad6d9791558d009053abb0eb8a
sharneo <irani.afshin@gmail.com>
2025-11-25 17:29:19 -0800

    Additional Commit

---
+++ models/curated/gwpc/business_critical/pc_policy.sql
@@ -0,0 +1,27 @@
+{#-
+
+Project: Data Uplift Progran 
+Project Description/Purpose: Data Uplift Program 
+
+Date            Version         Author          Description of Change           
+2025-11-18      0.0                             This Build the pc_policy in the curated layer
+
+-#}   
+
+{{ config(
+    materialized='incremental',
+    unique_key='id_hash',  
+    incremental_strategy='merge'  
+) }}
+
+{# --- Select all columns except the snapshot fields and hash columns --- #}
+SELECT
+    {{ dbt_utils.star(
+        from=ref('vw_pc_policy')
+    ) }}
+FROM {{ ref('vw_pc_policy') }} AS gwpc
+
+{% if is_incremental() %}
+  -- Only load new or updated rows in incremental runs
+  WHERE gwpc.updated_at > (SELECT MAX(updated_at) FROM {{ this }})
+{% endif %}
