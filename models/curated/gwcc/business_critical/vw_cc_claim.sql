15c08263fd8fd6f4f08af711c1f9bec066d290bd
sharneo <irani.afshin@gmail.com>
2025-11-26 03:14:05 -0800

    I am completing my build for now

---
+++ models/curated/gwcc/business_critical/vw_cc_claim.sql
@@ -0,0 +1,20 @@
+{#-
+
+Project: Data Uplift Progran 
+Project Description/Purpose: Data Uplift Program 
+
+Date            Version         Author          Description of Change           
+2025-11-18      0.0                             This Build the pc_policy in the curated layer
+
+-#}   
+
+{{ config(
+    materialized='view',
+) }}
+
+{# --- Select all columns except the snapshot fields and hash columns --- #}
+SELECT
+    {{ dbt_utils.star(
+        from=ref('cc_claim')
+    ) }}
+FROM {{ ref('cc_claim') }} 
