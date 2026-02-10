329bdb3fa454b50dd5a1756a83a26c258ee485d1
shareneo <irani.afshin@gmail.com>
2026-02-10 21:36:51 +1100

    I am done with RAW

---
+++ models/curated/gwbc/non_business_critical/bctl_basemoneyreceived.sql
@@ -0,0 +1,25 @@
+{#-
+Project: Data Uplift Program
+Project Description/Purpose: Data Uplift Program
+
+Date            Version         Author          Description of Change           
+2026.01.11      0.0                             This Creates a DBT Incremental Model for the Curated Layer
+-#}
+
+{{ config(
+    materialized='incremental',
+    incremental_strategy='merge',
+    unique_key='HASH_KEY',
+    tags=['daily', 'curated', 'hourly', 'billing_center']
+) }}
+
+SELECT
+    {{ dbt_utils.star(from=ref('vw_raw_bctl_basemoneyreceived')) }}
+FROM {{ ref('vw_raw_bctl_basemoneyreceived') }} AS src
+
+{% if is_incremental() %}
+WHERE src.file_ingestion_timestamp >= (
+    SELECT COALESCE(MAX(file_ingestion_timestamp), '1900-01-01')
+    FROM {{ this }}
+)
+{% endif %}
