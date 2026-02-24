1d2ca786b41982a00dc44e2cc5c0f8c6ba6edd64
shareneo <irani.afshin@gmail.com>
2026-02-24 21:04:59 +1100

    I am done

---
+++ models/curated/gwpc/views/vw_pctl_upgradedbstoragesettype_current.sql
@@ -0,0 +1,26 @@
+
+{#-
+Project: Data Uplift Program
+Project Description/Purpose: Data Uplift Program
+
+Date            Version         Author          Description of Change           
+2026.01.11      0.0                             This Creates a Current View for the Curated Layer for the Table  
+                                                pctl_upgradedbstoragesettype . This view contains only the Latest Record Data in the Table
+                                                This View is an Initial One and will need to be revisited. 
+                                                1 and 0 Represent Initial Initialisation from CDA
+-#}
+
+{{ config(
+    materialized='view',
+    tags=["daily", "curated","curated_view", "hourly", "policy"]
+) }}
+
+SELECT
+    {{ dbt_utils.star(from=ref('pctl_upgradedbstoragesettype')  ) }}
+FROM {{ ref('pctl_upgradedbstoragesettype') }}
+WHERE FILE_TYPE='PARQUET'
+      AND GWCBI_OPERATION NOT IN ('0','1')
+QUALIFY ROW_NUMBER() OVER (
+     PARTITION BY ID 
+     ORDER BY GWCBI_SEQVAL DESC 
+) = 1
