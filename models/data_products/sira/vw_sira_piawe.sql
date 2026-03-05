7a959640f7c435e102f6cf6106630a51366db7c1
Afshin Irani <afshin.irani@icare.nsw.gov.au>
2026-03-05 02:34:47 +0000

    Initial Commit

--- models/data_products/sira/vw_sira_piawe.sql
+++ models/data_products/sira/vw_sira_piawe.sql
@@ -20,8 +20,8 @@ with cc_exposure as (
         claimid,
         exposuretype,
         retired
-        from {{ ref('vw_cc_exposure_current') }}
-        where retired = 0
+    from {{ ref('vw_cc_exposure_current') }}
+    where retired = 0
 ),
 
 ccx_piawe_icare as (
@@ -41,7 +41,7 @@ ccx_piawe_icare as (
         rpenddate,
         draft
     from {{ ref('vw_ccx_piawe_icare_current') }}
-      where retired = 0
+    where retired = 0
 ),
 
 cctl_piawetype_icare as (
@@ -59,6 +59,8 @@ cctl_exposuretype as (
     from {{ ref('vw_cctl_exposuretype_current') }}
     where retired = 0
 ),
+
+
 {#-
 ccx_benefitsaccrual_icare as (
     select
@@ -73,9 +75,11 @@ cc_claim as (
         id,
         claimnumber,
         lossdate
-    from {{ ref('vw_cc_claim_current') }}    
-      where retired = 0
+    from {{ ref('vw_cc_claim_current') }}
+    where retired = 0
 ),
+
+
 {#-
 submission_period as (
     select submission_period_end_dt
@@ -83,78 +87,107 @@ submission_period as (
 ),
 -#}
 base as (
-    select 
+    select
         c.claimnumber,
         e.claimid,
         et.typecode,
---        ba.totalweekspaid,
-        case 
-            when cast(c.lossdate as date) <= '2018-10-25' then
-                case 
-                    when t.name in ('Calculated PIAWE', 'Manual PIAWE', 'Migrated PIAWE') then coalesce(p.ordinaryearnings_icare, 0) + coalesce(p.totalshiftweek, 0) + coalesce(p.totalovertimeweek, 0)
-                    when t.name in ('Interim PIAWE', 'Transitional Rate') then p.piawefirst52_icare
-             --       when t.name = 'Indexed PIAWE' then case when ba.totalweekspaid <= 52 then p.piawefirst52_icare else p.piawelater52_icare end
-                    when t.name in ('WCC PIAWE', 'Agreement PIAWE') then 0
-                end
-            when cast(c.lossdate as date) between '2018-10-26' and '2019-10-20' then
-                case 
-                    when t.name in ('Calculated PIAWE', 'Manual PIAWE', 'Migrated PIAWE') then coalesce(p.ordinaryearnings_icare, 0) + coalesce(p.totalshiftweek, 0) + coalesce(p.totalovertimeweek, 0)
-                    when t.name in ('Interim PIAWE', 'Transitional Rate', 'Indexed PIAWE') then p.piawefirst52_icare
-                    when t.name in ('WCC PIAWE', 'Agreement PIAWE') then 0
-                end
-            when cast(c.lossdate as date) >= '2019-10-21' then p.piawefirst52_icare
-        end as pre_injuiry_average_weekly,
-        case 
-            when cast(c.lossdate as date) <= '2018-10-25' then
-                case 
-                    when t.name in ('Calculated PIAWE', 'Manual PIAWE', 'Migrated PIAWE') then p.ordinaryearnings_icare
-                    when t.name in ('Interim PIAWE', 'Transitional Rate') then p.piawefirst52_icare
-            --        when t.name = 'Indexed PIAWE' then case when ba.totalweekspaid <= 52 then p.piawefirst52_icare else p.piawelater52_icare end
-                    when t.name in ('WCC PIAWE', 'Agreement PIAWE') then 0
-                end
-            when cast(c.lossdate as date) between '2018-10-26' and '2019-10-20' then
-                case 
-                    when t.name in ('Calculated PIAWE', 'Manual PIAWE', 'Migrated PIAWE') then p.ordinaryearnings_icare
-                    when t.name in ('Interim PIAWE', 'Transitional Rate', 'Indexed PIAWE') then p.piawefirst52_icare
-                    when t.name in ('WCC PIAWE', 'Agreement PIAWE') then 0
-                end
-            when cast(c.lossdate as date) >= '2019-10-21' then 0
-        end as ordinary_earnings,
-        case 
-            when cast(c.lossdate as date) <= '2018-10-25' or cast(c.lossdate as date) between '2018-10-26' and '2019-10-20' then
-                case 
-                    when t.name in ('Calculated PIAWE', 'Indexed PIAWE', 'Manual PIAWE', 'Migrated PIAWE') then p.totalshiftweek
-                    when t.name in ('WCC PIAWE', 'Agreement PIAWE', 'Interim PIAWE', 'Transitional Rate') then 0
-                end
-            when cast(c.lossdate as date) >= '2019-10-21' then 0
-        end as shift_allowance,
-        case 
-            when cast(c.lossdate as date) <= '2018-10-25' or cast(c.lossdate as date) between '2018-10-26' and '2019-10-20' then
-                case 
-                    when t.name in ('Calculated PIAWE', 'Indexed PIAWE', 'Manual PIAWE', 'Migrated PIAWE') then p.totalovertimeweek
-                    when t.name in ('WCC PIAWE', 'Agreement PIAWE', 'Interim PIAWE', 'Transitional Rate') then 0
-                end
-            when cast(c.lossdate as date) >= '2019-10-21' then 0
-        end as overtime,
+        --        ba.totalweekspaid,
         p.createtime,
         p.effectivedate_icare,
         p.piawedeactivated_icare,
         p.rpstartdate,
         p.rpenddate,
         p.draft,
-        row_number() over (partition by p.exposureid order by p.createtime desc, p.effectivedate_icare desc) as row_num,
-        p.exposureid
-    from cc_exposure e
-    left join ccx_piawe_icare p on e.id = p.exposureid
-    left join cctl_piawetype_icare t on p.piawetype_icare = t.id
-    left join cctl_exposuretype et on et.id = e.exposuretype
---    left join ccx_benefitsaccrual_icare ba on ba.exposureid = e.id
-    left join cc_claim c on c.id = e.claimid
---    cross join submission_period sp
-    where e.retired = 0
-      and et.typecode = 'LostWages'
-      and p.draft = 0
-      and p.piawedeactivated_icare = 0
+        p.exposureid,
+        case
+            when cast(c.lossdate as date) <= '2018-10-25'
+                then
+                    case
+                        when
+                            t.name in ('Calculated PIAWE', 'Manual PIAWE', 'Migrated PIAWE')
+                            then
+                                coalesce(p.ordinaryearnings_icare, 0)
+                                + coalesce(p.totalshiftweek, 0)
+                                + coalesce(p.totalovertimeweek, 0)
+                        when t.name in ('Interim PIAWE', 'Transitional Rate') then p.piawefirst52_icare
+                        --       when t.name = 'Indexed PIAWE' then case when ba.totalweekspaid <= 52 then p.piawefirst52_icare else p.piawelater52_icare end
+                        when t.name in ('WCC PIAWE', 'Agreement PIAWE') then 0
+                    end
+            when cast(c.lossdate as date) between '2018-10-26' and '2019-10-20'
+                then
+                    case
+                        when
+                            t.name in ('Calculated PIAWE', 'Manual PIAWE', 'Migrated PIAWE')
+                            then
+                                coalesce(p.ordinaryearnings_icare, 0)
+                                + coalesce(p.totalshiftweek, 0)
+                                + coalesce(p.totalovertimeweek, 0)
+                        when t.name in ('Interim PIAWE', 'Transitional Rate', 'Indexed PIAWE') then p.piawefirst52_icare
+                        when t.name in ('WCC PIAWE', 'Agreement PIAWE') then 0
+                    end
+            when cast(c.lossdate as date) >= '2019-10-21' then p.piawefirst52_icare
+        end as pre_injuiry_average_weekly,
+        case
+            when cast(c.lossdate as date) <= '2018-10-25'
+                then
+                    case
+                        when
+                            t.name in ('Calculated PIAWE', 'Manual PIAWE', 'Migrated PIAWE')
+                            then p.ordinaryearnings_icare
+                        when t.name in ('Interim PIAWE', 'Transitional Rate') then p.piawefirst52_icare
+                        --        when t.name = 'Indexed PIAWE' then case when ba.totalweekspaid <= 52 then p.piawefirst52_icare else p.piawelater52_icare end
+                        when t.name in ('WCC PIAWE', 'Agreement PIAWE') then 0
+                    end
+            when cast(c.lossdate as date) between '2018-10-26' and '2019-10-20'
+                then
+                    case
+                        when
+                            t.name in ('Calculated PIAWE', 'Manual PIAWE', 'Migrated PIAWE')
+                            then p.ordinaryearnings_icare
+                        when t.name in ('Interim PIAWE', 'Transitional Rate', 'Indexed PIAWE') then p.piawefirst52_icare
+                        when t.name in ('WCC PIAWE', 'Agreement PIAWE') then 0
+                    end
+            when cast(c.lossdate as date) >= '2019-10-21' then 0
+        end as ordinary_earnings,
+        case
+            when
+                cast(c.lossdate as date) <= '2018-10-25'
+                or cast(c.lossdate as date) between '2018-10-26' and '2019-10-20'
+                then
+                    case
+                        when
+                            t.name in ('Calculated PIAWE', 'Indexed PIAWE', 'Manual PIAWE', 'Migrated PIAWE')
+                            then p.totalshiftweek
+                        when t.name in ('WCC PIAWE', 'Agreement PIAWE', 'Interim PIAWE', 'Transitional Rate') then 0
+                    end
+            when cast(c.lossdate as date) >= '2019-10-21' then 0
+        end as shift_allowance,
+        case
+            when
+                cast(c.lossdate as date) <= '2018-10-25'
+                or cast(c.lossdate as date) between '2018-10-26' and '2019-10-20'
+                then
+                    case
+                        when
+                            t.name in ('Calculated PIAWE', 'Indexed PIAWE', 'Manual PIAWE', 'Migrated PIAWE')
+                            then p.totalovertimeweek
+                        when t.name in ('WCC PIAWE', 'Agreement PIAWE', 'Interim PIAWE', 'Transitional Rate') then 0
+                    end
+            when cast(c.lossdate as date) >= '2019-10-21' then 0
+        end as overtime,
+        row_number() over (partition by p.exposureid order by p.createtime desc, p.effectivedate_icare desc) as row_num
+    from cc_exposure as e
+    left join ccx_piawe_icare as p on e.id = p.exposureid
+    left join cctl_piawetype_icare as t on p.piawetype_icare = t.id
+    left join cctl_exposuretype as et on e.exposuretype = et.id
+    --    left join ccx_benefitsaccrual_icare ba on ba.exposureid = e.id
+    left join cc_claim as c on e.claimid = c.id
+    --    cross join submission_period sp
+    where
+        e.retired = 0
+        and et.typecode = 'LostWages'
+        and p.draft = 0
+        and p.piawedeactivated_icare = 0
 --    and p.effectivedate_icare <= sp.submission_period_end_dt
 )
 
