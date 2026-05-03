e51f87e6e5d0f5fada507a61968c043160121d78
shareneo <irani.afshin@gmail.com>
2026-05-03 12:32:51 +1000

    I am done

---
+++ models/data_products/aspire/msc_qlk_aspire_accident.sql
@@ -0,0 +1,148 @@
+-- models/msc_qlk_aspire_accident.sql
+
+with
+
+cc_claim as (
+    select
+          id
+        , claimnumber
+        , locationcodeid
+        , claimworkcompid
+        , retired
+    from {{ ref('v_cc_claim_current') }}
+    where retired = 0
+),
+
+cc_incident as (
+    select
+          claimid
+        , claimincident
+        , retired
+        , mechanismofinjurydesc_icare
+        , subtype
+    from {{ ref('v_cc_incident_current') }}
+    where claimincident = 1
+      and retired = 0
+),
+
+cctl_incident as (
+    select
+          id
+        , typecode
+    from {{ ref('v_cctl_incident_current') }}
+    where lower(typecode) = 'injuryincident'
+),
+
+cc_policylocation as (
+    select
+          id
+        , retired
+        , addressid
+    from {{ ref('v_cc_policylocation_current') }}
+    where retired = 0
+),
+
+cc_address as (
+    select
+          id
+        , retired
+        , addressline1
+        , addressline2
+        , addressline3
+        , city
+        , postalcode
+    from {{ ref('v_cc_address_current') }}
+    where retired = 0
+),
+
+cc_workcomp as (
+    select
+          id
+        , retired
+        , accidentlocationtype_icare
+    from {{ ref('v_cc_workcomp_current') }}
+    where retired = 0
+),
+
+cctl_accidentloctype_icare as (
+    select
+          id
+        , typecode
+        , name
+    from {{ ref('v_cctl_accidentloctype_icare_current') }}
+),
+
+cc_subrogationsummary as (
+    select
+          id
+        , claimid
+        , retired
+    from {{ ref('v_cc_subrogationsummary_current') }}
+    where retired = 0
+),
+
+cc_subrogation as (
+    select
+          subrogationsummaryid
+        , retired
+    from {{ ref('v_cc_subrogation_current') }}
+    where retired = 0
+),
+
+subro as (
+    select distinct 
+        subrosumm.claimid
+    from cc_subrogationsummary subrosumm 
+    inner join cc_subrogation subro
+        on subro.subrogationsummaryid = subrosumm.id
+),
+
+final as (
+    select distinct
+          convert(varchar(32), hashbytes('md5', concat('GWCC', clm.claimnumber)), 2) as claim_sk
+        , 'GWCC' as src_system_cd
+        , clm.id as src_claim_id
+        , clm.claimnumber as claim_nbr
+        , case 
+            when subro.claimid is not null then 'Y' 
+            else 'N' 
+          end as recovery_investigation_ind
+        , concat(
+              rtrim(
+                  concat(pollocaddr.addressline1, ' '
+                       , pollocaddr.addressline2, ' '
+                       , pollocaddr.addressline3
+                  )
+              )
+            , ' ', pollocaddr.city, ' '
+            , pollocaddr.postalcode
+          ) as policy_location_addr
+        , inc.mechanismofinjurydesc_icare as toocs_mechanism_if_injury_desc
+        , dimacc.typecode as accident_location_type_cd
+        , dimacc.name as accident_location_type_desc
+
+    from cc_claim clm
+
+    join cc_incident inc
+        on clm.id = inc.claimid
+
+    join cctl_incident cctl_incident
+        on cctl_incident.id = inc.subtype
+
+    left join cc_policylocation polloc
+        on clm.locationcodeid = polloc.id
+
+    join cc_address pollocaddr
+        on polloc.addressid = pollocaddr.id
+
+    left join cc_workcomp wrkcomp
+        on clm.claimworkcompid = wrkcomp.id
+
+    join cctl_accidentloctype_icare dimacc
+        on wrkcomp.accidentlocationtype_icare = dimacc.id
+
+    left join subro
+        on subro.claimid = clm.id
+)
+
+select * from final
