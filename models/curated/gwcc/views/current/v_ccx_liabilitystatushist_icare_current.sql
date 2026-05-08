8d80eba762348796bbbb736c5f3f73011dae644a
shareneo <irani.afshin@gmail.com>
2026-05-08 12:04:43 +1000

    I am done

---
+++ models/curated/gwcc/views/current/v_ccx_liabilitystatushist_icare_current.sql
@@ -0,0 +1,32 @@
+{#-
+Project: Data Uplift Program
+Date            Version         Author          Description of Change           
+2026-01-01      0.5                             Curated current-state view for ccx_liabilitystatushist_icare.
+                                                Filters: current SCD2 via {{ snapshot_valid_to_current() }}
+                                                         excludes Guidewire Cloud Program delete Flag i.e. CDA Deletion is 1 and snapshot hard deletes
+-#}
+
+{{ config(
+    materialized='view',
+    tags=["current_view"]
+) }}
+
+with source as (
+
+    select * from {{ ref('ccx_liabilitystatushist_icare_snapshot') }}
+
+),
+
+filtered as (
+
+    select
+        source.*,
+        'Y' as current_record
+    from source
+    where valid_to = {{ snapshot_valid_to_current() }}
+      and coalesce(gwcbi_operation, 0) <> 1
+      and lower(is_deleted) = 'false'
+
+)
+
+select * from filtered
